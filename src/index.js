2834b3af799ff126081c5e063644473977f8b4a3
Nicholas Kubo <nickskubo@gmail.com>
2021-02-24 20:00:10 -0800

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+form = document.querySelector(`#create-task-form`);
+let commentContainer = document.querySelector(`#tasks`)
+
+form.addEventListener(`submit`, (e) => {
+  e.preventDefault();
+  let li = document.createElement(`li`) 
+  li.textContent= e.target[`new-task-description`].value;
+  commentContainer.appendChild(li);
+  button = document.createElement(`button`);
+  button.textContent = `X`;
+  li.appendChild(button);
+  button.style.background = "red"; 
+  button.addEventListener(`click`, (e) =>{
+  e.target.parentElement.remove(li);
+  })
+})
